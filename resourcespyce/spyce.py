03af5fb0da4507a8d109cedc4923a01eb44a417b
fletchgraham <sfletchgraham@gmail.com>
2020-04-23 21:06:48 -0400

    start writing the Spyce class. implement the search api function.

---
+++ resourcespyce/spyce.py
@@ -0,0 +1,18 @@
+import requests
+import hashlib
+
+class Spyce:
+    def __init__(self, base_url='', user='admin', private_key=''):
+        self.base_url = base_url
+        self.user = user
+        self.private_key = private_key
+
+    def do_search(self, param1):
+        query = f'user={self.user}&function=do_search&param1={param1}'
+        sign = hashlib.sha256(
+            f'{self.private_key}{query}'.encode()
+            ).hexdigest()
+
+        res = requests.get(f'{self.base_url}{query}&sign={sign}')
+
+        return res.json()
